f1cab786fa627d8e2a736724b1fcee2cb37f5907
Erwin <erwin.paralesda@gmail.com>
2022-08-30 19:01:19 -0500

    Add files via upload

---
+++ CatalogAPI/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using CatalogAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogAPI
+{
+    public static class Extensions
+    {
+
+        public static ItemDto AsDto(this Item item) 
+        {
+            return new ItemDto(item.Id, item.Name, item.Description, item.Price, item.CreatedDate);
+        }
+    }
+}
---
+++ CatalogAPI/Settings/MongoDbSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogAPI.Settings
+{
+    public class MongoDbSettings
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+
+        public string ConnectionString 
+        {
+            get 
+            {
+                return $"mongodb://{Host}:{Port}";
+            }
+        }
+
+    }
+}
